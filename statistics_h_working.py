d2ce22183a595cadb136d6da94156006aa4fc672
ksaslam <pawnoutlet@pawns-mbp-2.ceri.memphis.edu>
2017-06-14 12:18:55 -0500

    The latest analysis tools with h value to use

---
+++ statistics_h_working.py
@@ -0,0 +1,215 @@
+from __future__ import print_function
+import numpy as np
+#import sys
+#sys.path.append('/Users/pawnoutlet/Documents/fdfault/data')
+import fdfault
+#import seistools.coulomb
+from coulomb import coulomb_2d
+#import matplotlib.pyplot as plt
+
+# defining some parameters to use it later
+
+prob_name= 'seed'              # parameters related to problem
+roughness='h' + '0_6'
+RMS= '0_001'
+number_of_realization=100
+
+receiver_fault_orientation= np.array([0,1])    # parameters related to coloumb stress
+mu=0.4
+coloumbs_stress_time_step= 16                       # Time step at which stresses are required  	     
+
+#range_values=np.zeros([100,2])
+
+
+distance_start= 15000.              # Since fault is at 20 km so I am taking 5 km distance on both sides
+distance_end= 20000                 
+space_interval=50                  # distance between two grid points 
+distance_start2= 25000      # the second array will start from here
+
+index_y_start= int(distance_start/space_interval)    # 
+index_y_end= int(distance_end/space_interval)
+index_y_start2=  int(distance_start2/space_interval)
+range_of_iter=index_y_end-index_y_start +1
+print(index_y_start, index_y_end, index_y_start2, range_of_iter)
+
+
+distance_array= np.linspace( (distance_end-distance_start )/1000., 0, range_of_iter) 
+total_dim_x = 801
+
+
+cff_w_distanc1 = np.zeros(( range_of_iter,number_of_realization* total_dim_x) )   
+cff_w_distanc2= np.zeros(( range_of_iter,number_of_realization* total_dim_x) )
+rupture_length= np.zeros((number_of_realization))
+
+
+
+
+string= 'sxxbody.' +'sxx'+'.shape[0]'   # to find the time step 
+
+
+
+summ_switch= 0
+start_x= 400
+for kk in range(number_of_realization):
+	if (kk!=9 and kk!=10):	
+	# loading all the data for each realization
+		print(kk)
+		problem_name= prob_name + str(kk) +roughness+ 'RMS'+ RMS 
+		sxxbody = fdfault.output(problem_name,'sxx'+'body')
+		sxxbody.load()
+		sxybody = fdfault.output(problem_name,'sxy'+'body')
+		sxybody.load()
+		syybody = fdfault.output(problem_name,'syy'+'body')
+		syybody.load()
+
+		# this part is for the patch length
+		vfault = fdfault.output(problem_name,'v'+'fault')
+		vfault.load()	
+		
+
+		print(problem_name)
+
+
+		
+		# Need to calculate the patch length that is rupturing
+		
+		fault_dimension= np.linspace(0,80., vfault.V.shape[1])
+		rupt_indx= vfault.V.argmax(axis=1)
+		rupt_slip_rate= vfault.V.max(axis=1)
+
+
+		rupt_slip_test= vfault.V[coloumbs_stress_time_step, :]   # store all the values in this array
+		print(rupt_slip_test.shape, 'the shape of new array')
+		max_slip_side1= np.max(rupt_slip_test[rupt_indx[0] :vfault.V.shape[1] ])
+		max_slip_side2= np.max(rupt_slip_test[0: rupt_indx[0]  ])
+		indx_max_slip_side1 = np.where(rupt_slip_test== max_slip_side1)
+	#	print('max slip', max_slip_side1, max_slip_side2, indx_max_slip_side1)
+
+		if  max_slip_side1>= 10.0:
+			indx_max_slip_side1 = np.where(max_slip_side1)
+		else:
+			for runs in range (coloumbs_stress_time_step-3):
+			
+				rupt_slip_test= vfault.V[coloumbs_stress_time_step- 1- runs, :]
+				max_slip_side1= np.max(rupt_slip_test[rupt_indx[0] :vfault.V.shape[1] ])
+				if  max_slip_side1>= 10.0:
+					indx_max_slip_side1 = np.where(rupt_slip_test== max_slip_side1)
+					break
+				else:
+					indx_max_slip_side1 = 	rupt_indx[0]	
+
+		if  max_slip_side2>= 10.0:
+			indx_max_slip_side2 = np.where(max_slip_side2)
+		else:
+			for runs in range (coloumbs_stress_time_step-3):
+			
+				rupt_slip_test= vfault.V[coloumbs_stress_time_step- 1- runs, :]
+				max_slip_side2= np.max(rupt_slip_test[0:rupt_indx[0] ])
+				if  max_slip_side2>= 10.0:
+					indx_max_slip_side2 = np.where(rupt_slip_test== max_slip_side2)
+					break
+				else:
+					indx_max_slip_side2 = 	rupt_indx[0]		
+
+		rupture_length[kk] = fault_dimension[indx_max_slip_side1] - fault_dimension [indx_max_slip_side2]
+	#	print('rupture length', rupture_length)
+		#print('max slip', max_slip_side1, max_slip_side2)
+
+		# if  rupt_slip_rate[coloumbs_stress_time_step]   >= 10.0:
+		# 	rupture_length[kk]= fault_dimension [  rupt_indx[coloumbs_stress_time_step]  ]-  fault_dimension [ rupt_indx[0] ]
+		# else:
+		# 	for runs in range (coloumbs_stress_time_step-3):
+		# 		if rupt_slip_rate [coloumbs_stress_time_step- 1- runs]   >= 10.0:
+		# 			rupture_length[kk]= fault_dimension [  rupt_indx[coloumbs_stress_time_step-1- runs]  ]-  fault_dimension [ rupt_indx[0] ]
+		# 			break
+		# 		else:
+		# 			rupture_length[kk] =0
+						
+
+		
+
+
+
+
+
+		total_time= eval(string)
+	#	k= 2
+		k= coloumbs_stress_time_step
+		c_stress=coulomb_2d(sxxbody.sxx[k,:,:], sxybody.sxy[k,:,:], syybody.syy[k,:,:], receiver_fault_orientation, mu)
+		# coulomb_min= c_stress.min()
+		# coulomb_min= c_stress.max()
+		
+	#	print(c_stress.shape)
+
+
+		
+		for dim_y in range(range_of_iter):                 # populating the values of the stresses in a matrix, the matrix row is disntace and coloumn is value
+			for dim_x in range(total_dim_x):
+				cff_w_distanc1[dim_y, summ_switch* total_dim_x +dim_x]= c_stress[start_x+ dim_x, dim_y+index_y_start]    # one row is for one distance and the second row is for second disntace
+				cff_w_distanc2[dim_y, summ_switch* total_dim_x +dim_x]= c_stress[start_x+ dim_x, index_y_start2-dim_y]
+			
+			
+			
+
+		summ_switch= summ_switch +1  	       
+
+cff_full= np.concatenate((cff_w_distanc1, cff_w_distanc2), axis=1)
+#print(cff_full.shape)
+#print(cff_w_distanc1.shape)
+
+print(cff_full.shape)
+print(cff_w_distanc1.shape)
+np.savetxt('statistics_stress_'+ roughness+'RMS' + RMS +'.out', cff_w_distanc1 , delimiter=',')
+np.savetxt('rupture_length_'+ roughness+'RMS'+ RMS +'.out', rupture_length , delimiter=',')
+#print(cff_w_distanc1[0,:])
+#np.savetxt('statistics_stress.out', cff_w_distanc1 , delimiter=',')
+
+#plt.hist(cff_w_distanc1 [0,total_dim_x:2*total_dim_x], bins=80)
+#plt.hist(cff_full[0,2*total_dim_x:3*total_dim_x], bins=80)
+#plt.show()
+
+
+# for xe, ye in zip(distance_array, cff_full):
+    
+# #    plt.scatter([xe] * len(ye), ye, c= ye , cmap='viridis')
+#     plt.scatter([xe] * len(ye), ye, facecolor='0.5')
+#plt.colorbar()
+#plt.axis([0, 3, -80, 80])
+# plt.show()
+
+#plt.plot(distance_array, cff_w_distanc1 [:, 0:total_dim_x], 'o')
+#plt.axis([-1, 5, -30, 30])
+#plt.show()
+
+
+#plt.hist(cff_full [100, :], bins=50 )
+#plt.hist(cff_full [100, :])
+
+
+
+
+# range_plot= int(range_of_iter/3.0) +1
+# for ii in range(range_plot):
+# 	plt.figure(ii)
+# 	i= ii*2+1
+# #	print(cff_full [ii, 0:2*total_dim_x])
+# 	plt.hist(cff_full [i, 0:2*total_dim_x], bins=100 )
+# 	plt.savefig(str(ii) +'.png') 
+
+
+
+#plt.colorbar()
+
+#	plt.xlabel('Position along the fault (km)', fontsize=16, color='black')
+#	plt.ylabel('Position accross the strike (km)', fontsize=16, color='black')
+
+
+
+	
+
+
+
+
+
+
+
---
+++ zones_rms.py
@@ -0,0 +1,214 @@
+from __future__ import print_function
+import numpy as np
+import sys
+sys.path.append('/Users/pawnoutlet/Documents/fdfault/data')
+import fdfault
+import seistools.coulomb
+#from coulomb import coulomb_2d
+import matplotlib.pyplot as plt
+
+
+
+
+
+
+# defining some parameters to use it later
+
+prob_name = 'seed'              # parameters related to problem
+roughness = 'h' + '0_6'
+RMS='0_001'
+number_of_realization=2
+
+
+receiver_fault_orientation= np.array([0,1])    # parameters related to coloumb stress
+mu=0.4
+coloumbs_stress_time_step= 11                     # Time step at which stresses are required  	     
+const_stress= -2.5                                   # the separating stress used to define +ve and -ve zones
+#range_values=np.zeros([100,2])
+
+
+              # Since fault is at 20 km so I am taking 5 km distance on both sides
+#distance_start= 20000                 
+distance_start= 18000
+distance_end= 20000  
+space_interval=50
+#index_y_start= int(distance_start/space_interval)-10    # 10 grid points away from the fault 
+index_y_start= int(distance_start/space_interval)
+index_y_end= int(distance_end/space_interval)-10
+range_of_iter=index_y_end-index_y_start +1
+#range_of_iter= 1
+total_dim_x = 801
+
+distance_array= np.linspace( 5000, 500, range_of_iter )
+#print(distance_array) 
+
+#cff_w_distanc1 = np.zeros(( number_of_realization, total_dim_x) )   
+
+string= 'sxxbody.' +'sxx'+'.shape[0]'   # to find the time step 
+
+
+
+
+start_x= 400
+
+positive_zones= np.zeros((range_of_iter, number_of_realization))
+negative_zones=np.zeros((range_of_iter,  number_of_realization))
+each_pos_zone_len= np.zeros((number_of_realization, range_of_iter, 50))
+each_neg_zone_len= np.zeros((number_of_realization, range_of_iter, 50))
+
+
+for kk in range(number_of_realization):
+#	if(kk==6):	
+		# loading all the data for each realization
+		print(kk)
+		problem_name= prob_name + str(kk) + roughness + 'RMS'+ RMS 
+		sxxbody = fdfault.output(problem_name,'sxx'+'body')
+		sxxbody.load()
+		sxybody = fdfault.output(problem_name,'sxy'+'body')
+		sxybody.load()
+		syybody = fdfault.output(problem_name,'syy'+'body')
+		syybody.load()
+		#print(problem_name)
+		
+		total_time= eval(string)
+	#	k= 2
+		k= coloumbs_stress_time_step
+		c_stress=seistools.coulomb.coulomb_2d(sxxbody.sxx[k,:,:], sxybody.sxy[k,:,:], syybody.syy[k,:,:], receiver_fault_orientation, mu)
+		# coulomb_min= c_stress.min()
+		# coulomb_min= c_stress.max()
+		
+	#	print(c_stress.shape
+		for dim_y in range(range_of_iter):	
+			for dim_x in range(total_dim_x):
+
+				if c_stress[start_x+ dim_x, index_y_start + dim_y ]>= const_stress and c_stress[start_x+ dim_x+1, index_y_start + dim_y ] <const_stress :
+						print('inside the loop +')				
+						if  c_stress[start_x+ dim_x+2, index_y_start + dim_y] <const_stress  and c_stress[start_x+ dim_x+3, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+4, index_y_start+ dim_y]<const_stress and c_stress[start_x+ dim_x+5, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+6, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+7, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+8, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+9, index_y_start + dim_y] <const_stress and c_stress[start_x+ dim_x+10, index_y_start + dim_y] <const_stress:
+
+							negative_zones[dim_y, kk] = negative_zones[dim_y, kk] +1
+							
+							neg_zone= 0
+							for runs in range (100):
+								if c_stress [start_x+ dim_x+1+runs , index_y_start + dim_y] < const_stress:
+									neg_zone= neg_zone +1
+									neg_zone_len= neg_zone * 50.
+									each_neg_zone_len[kk, dim_y ,int( negative_zones[dim_y,kk]-1 )] = neg_zone_len
+
+								else:
+									break	
+							
+									
+
+
+
+
+				if c_stress[start_x+ dim_x, index_y_start + dim_y] < const_stress and c_stress[start_x+ dim_x+1, index_y_start + dim_y] >= const_stress :
+						print('inside the loop -')				
+						if  c_stress[start_x+ dim_x+2, index_y_start + dim_y] >= const_stress  and c_stress[start_x+ dim_x+3, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+4, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+5, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+6, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+7, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+8, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+9, index_y_start + dim_y] >= const_stress and c_stress[start_x+ dim_x+10, index_y_start + dim_y] >= const_stress:
+				
+							positive_zones[dim_y, kk] = positive_zones[dim_y, kk] +1
+
+							pos_zone= 0
+							for runs in range (100):
+								if c_stress [start_x+ dim_x+1+runs , index_y_start + dim_y] >= const_stress:
+									pos_zone= pos_zone +1
+									pos_zone_len= pos_zone * 50.
+									each_pos_zone_len[kk,dim_y ,int( positive_zones[dim_y, kk]-1 )] = pos_zone_len
+								else:
+									break			
+															
+
+
+
+#print(negative_zones[range_of_iter-1,:])
+#print( each_neg_zone_len[0, range_of_iter-1 , :] )
+#print(each_neg_zone_len.shape)
+
+
+full_pos_len_zone_matrix=np.zeros((range_of_iter, number_of_realization*50 +1 ) )
+for jj in range( range_of_iter):
+	b= np.zeros((1))
+	for ii in range (number_of_realization):
+		a= each_pos_zone_len[ii, jj, :]
+		b= np.concatenate([a, b])             # this is just to concatenate all the values b, a are dummy
+	full_pos_len_zone_matrix[jj, :]= b	
+
+
+
+
+np.savetxt('positive_zones_' + roughness + 'RMS' +RMS +'.out', positive_zones , delimiter=',')
+np.savetxt('negative_zones_' + roughness + 'RMS'+ RMS +'.out', negative_zones , delimiter=',')
+np.savetxt('full_postv_zone_len_' + roughness + 'RMS'+RMS +'.out', full_pos_len_zone_matrix , delimiter=',')
+#np.savetxt('each_negtv_zone_len.out', each_neg_zone_len , delimiter=',')
+
+#plt.plot(distance_array, full_pos_len_zone_matrix[:, :], 'o')
+#plt.axis([-1, 5, -30, 30])
+#plt.show()
+
+#print(a.shape, b.shape, 'these are shapes of all ')	
+
+
+#plt.hist(positive_zones[0, :], bins= 15, facecolor='yellow', edgecolor='gray', label='Positive zones')
+#plt.hist(negative_zones[0, :], bins= 15, facecolor='red', edgecolor='gray', label='negative zones')
+#plt.xlabel(' # of zones')
+#plt.ylabel('# per bin')
+#plt.title('Pdf of stress zones', fontsize=16,color='black')
+#plt.legend( loc='upper right')
+#plt.show()
+
+#print(cff_full.shape)
+#print(cff_w_distanc1.shape)
+
+
+
+#plt.hist(cff_w_distanc1 [0,total_dim_x:2*total_dim_x], bins=80)
+#plt.hist(cff_full[0,2*total_dim_x:3*total_dim_x], bins=80)
+#plt.show()
+
+
+# for xe, ye in zip(distance_array, cff_full):
+    
+# #    plt.scatter([xe] * len(ye), ye, c= ye , cmap='viridis')
+#     plt.scatter([xe] * len(ye), ye, facecolor='0.5')
+#plt.colorbar()
+#plt.axis([0, 3, -80, 80])
+# plt.show()
+
+#plt.plot(distance_array, cff_w_distanc1 [:, 0:total_dim_x], 'o')
+#plt.axis([-1, 5, -30, 30])
+#plt.show()
+
+
+#plt.hist(cff_full [100, :], bins=50 )
+#plt.hist(cff_full [100, :])
+
+
+
+
+# range_plot= int(range_of_iter/3.0) +1
+# for ii in range(range_plot):
+# 	plt.figure(ii)
+# 	i= ii*2+1
+# #	print(cff_full [ii, 0:2*total_dim_x])
+# 	plt.hist(cff_full [i, 0:2*total_dim_x], bins=100 )
+# 	plt.savefig(str(ii) +'.png') 
+
+# plt.pcolor(c_stress[start_x:start_x+ total_dim_x, :], vmin= -10, vmax= 10)
+# plt.colorbar()
+# plt.show()
+
+#plt.colorbar()
+
+#	plt.xlabel('Position along the fault (km)', fontsize=16, color='black')
+#	plt.ylabel('Position accross the strike (km)', fontsize=16, color='black')
+
+
+
+	
+
+
+
+
+
+
+
